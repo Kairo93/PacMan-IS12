8c88ec9e5a05115a887fd833ffff6ea74acc3c9c
Kairo Kübard <kairo.kybard@gmail.com>
2013-11-06 14:55:39 +0200

    Elukad vol 2

--- elukad.py
+++ elukad.py
@@ -2,6 +2,7 @@ import time
 
 x = 100
 y = 2
+suund = 'down'
 
 def elukas_print():
 	elukas = "\033[" + str(y) + ";" + str(x) + "H$"
@@ -14,91 +15,104 @@ def elukas_next():
 	pacman = player_xy()
 	
 	px = pacman[0]
+	py = pacman[1]
 
 	# Search pacman from right
-	if pacman[0] > x and pacman[1] >= y:
+	if px >= x and py >= y:
 		# Look right
 		if kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
+			suund = 'right'
 		# Look left
 		elif kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
+			suund = 'left'
 		# Look up
 		elif kaart_xy(x, y-1) == 1:
 			x = x
 			y = y-1
+			suund = 'up'
 		# Look down
 		elif kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
+			suund = 'down'
 			
 
 	# Search pacman from left
-	if pacman[0] < x and pacman[1] == y:
+	if px <= x and py == y:
 		# Look left
 		if kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
+			suund = 'left'
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
+			suund = 'right'
 		# Look up
 		elif kaart_xy(x, y-1) == 1:
 			x = x
 			y = y-1
+			suund = 'up'
 		# Look down
 		elif kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
+			suund = 'down'
 			
 
 	# Search pacman from top
-	if pacman[0] == x and pacman[1] < y:
+	if px == x and py <= y:
 		# Look up
 		if kaart_xy(x, y-1) == 1:
 			x = x
 			y = y-1
+			suund = 'up'
 		# Look down
 		elif kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
+			suund = 'down'
 		# Look left
 		elif kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
+			suund = 'left'
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
+			suund = 'right'
 
 	# Search pacman from bottom
-	if pacman[0] == x and pacman[1] > y:
+	if px == x and py >= y:
 		# Look down
 		if kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
+			suund = 'down'
 		# Look up
 		elif kaart_xy(x, y-1) == 1:
 			x = x
 			y = y-1
+			suund = 'up'
 		# Look left
 		elif kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
+			suund = 'left'
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
+			suund = 'right'
 				
 
 
 def elukas_xy():
-	print "{};{}".format(x, y)
 	return [x, y]
 
-
-elukas_print()
-elukas_xy()
